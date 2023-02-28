8ed05750c7fbe63f0d77941d98190da7dd2c063e
AdascaliteiRobert <100295184+AdascaliteiRobert@users.noreply.github.com>
2023-02-28 16:42:22 +0200

    Add files via upload

---
+++ LAB2/LAB2/Road.java
@@ -0,0 +1,43 @@
+public class Road {
+    private double lenght;
+    private double speedLimit;
+    private RoadType type;
+
+    public Road(double lenght, double speedLimit, RoadType type) {
+        this.lenght = lenght;
+        this.speedLimit = speedLimit;
+        this.type = type;
+    }
+    public double getLenght() {
+        return lenght;
+    }
+
+    public void setLenght(double lenght) {
+        this.lenght = lenght;
+    }
+
+    public double getSpeedLimit() {
+        return speedLimit;
+    }
+
+    public void setSpeedLimit(double speedLimit) {
+        this.speedLimit = speedLimit;
+    }
+
+    public RoadType getType() {
+        return type;
+    }
+
+    public void setType(RoadType type) {
+        this.type = type;
+    }
+    @Override
+    public String toString() {
+        return "using a " + type +
+                " road " + " with the speed limit " +
+                speedLimit + " km/h "+
+                " and the lenght " +
+                lenght  + " km .";
+    }
+}
+
---
+++ LAB2/LAB2/StartingPoint.java
@@ -0,0 +1,13 @@
+public class StartingPoint extends Location {
+    public StartingPoint(double coordX, double coordY, String name, LocationType type) {
+        super(coordX, coordY, name, type);
+    }
+
+    @Override
+    public String toString() {
+        return "We are leaving the "+
+                getType() + " " + getName() +
+                " ( "+ getCoordX() +
+                " , " + getCoordY() + " ) ";
+    }
+}
